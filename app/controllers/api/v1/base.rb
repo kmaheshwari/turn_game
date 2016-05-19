2fed5dd7929c8c4cdf46c115f4fb4c92e8a06b52
kmaheshwari <maheshwari.kajol@gmail.com>
2016-05-19 16:03:39 +0530

    controllers generated

--- app/controllers/api/v1/base.rb
+++ app/controllers/api/v1/base.rb
@@ -3,6 +3,10 @@ module API
   module V1
     class Base < Grape::API
       	mount API::V1::Games
+      	mount API::V1::Categories
+      	mount API::V1::Leaderboards
+      	mount API::V1::PlaystoreUrls
+      	mount API::V1::Users
     end
   end
 end
---
+++ app/controllers/api/v1/playstore_urls.rb
@@ -0,0 +1,59 @@
+module API
+  module V1
+    class PlaystoreUrls < Grape::API
+    	include API::V1::Defaults
+    	resource :playstoreUrls do
+    	  desc "Return all playstoreUrls"
+    	  get "", root: :playstoreUrls do
+    	    PlaystoreUrl.all
+    	  end
+
+    	  desc "Return an playstoreUrl"
+    	  params do
+    	    requires :id, type: String, desc: "ID of the playstoreUrl"
+    	  end
+    	  get ":id", root: "playstoreUrl" do
+    	    PlaystoreUrl.where(id: permitted_params[:id]).first!
+    	  end
+    	  
+    	  desc "create a new playstoreUrl"
+    	  ## This takes care of parameter validation
+    	  params do
+    	    group :playstoreUrl, type: Hash do
+    	      optional :playstoreUrl_name, type: String
+    	      optional :playstore_url_id, type:Integer
+    	    end  
+    	  end
+    	  ## This takes care of creating playstoreUrl
+    	  post do
+    	    PlaystoreUrl.new(params[:playstoreUrl]).save!
+    	  end 
+
+    	  desc "delete an playstoreUrl"
+    	  ## This takes care of parameter validation
+    	  params do
+    	    requires :id, type:String, desc: "ID of the playstoreUrl"
+    	  end
+    	  ## This takes care of deleting playstoreUrl
+    	  delete ":id" do
+    	    PlaystoreUrl.find(params[:id]).destroy!  
+    	  end  
+
+    	  desc "update an playstoreUrl"
+    	  ## This takes care of parameter validation
+    	  params do
+    	    group :playstoreUrl, type: Hash do
+    	      optional :playstoreUrl_name, type: String
+    	      optional :playstore_url_id, type:Integer
+    	   
+    	    end  
+    	  end
+    	  ## This takes care of editing playstoreUrl
+    	  put ":id" do
+    	    PlaystoreUrl.find(params[:id]).update_attributes(params[:playstoreUrl])
+    	  end 
+
+    	end	
+    end
+  end
+end
